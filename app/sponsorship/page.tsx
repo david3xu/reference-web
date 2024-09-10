903d2ee9f5b039e63ec300197de3827c334b1082
David Curtin <291928k@curtin.edu.au>
2024-09-10 18:59:29 +0800

    Implement new feature

--- app/sponsorship/page.tsx
+++ app/sponsorship/page.tsx
@@ -1,13 +1,13 @@
-import NavigationBar from "@/components/NavigationBar";
-import Footer from "@/components/Footer";
-import Sponsorship from "@/components/Sponsorship";
+import NavigationBar from "@/components/NavigationBar/NavigationBar";
+import Footer from "@/components/Footer/Footer";
+// import Sponsorship from "@/components/Sponsorship";
 
 export default function Page() {
   return (
     <div className="flex flex-col min-h-screen">
       <NavigationBar />
 
-      <Sponsorship />
+      {/* <Sponsorship /> */}
 
       <Footer />
     </div>
